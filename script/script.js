8b803b1db185f425bbcc1a8f7d19ba665f950c42
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-16 17:20:57 +0100

    function "cellsGenerator"

--- script/script.js
+++ script/script.js
@@ -5,12 +5,19 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
 
+//* Functions
+const cellsGenerator = () => {
+    for (let i = 1; i <= 100; i++) cell += '<div class="cell"></div>';
+    return console.log(cell);
+}
+
 //* DOM's elemens
 const grid = document.getElementById('grid');
 const playButton = document.getElementById('play-button');
 
 //* Generating a div with class cell
 let cell = '';
-// Cycle to Render 100 cells 
-for (let i = 1; i <= 100; i++) cell += '<div class="cell"></div>';
-console.log(cell);
+
+playButton.addEventListener('click', function(){
+    cellsGenerator();
+})
